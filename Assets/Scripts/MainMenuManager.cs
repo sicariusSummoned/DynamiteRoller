b27ddb2683f3abb1396af49b7e8c2054e86c130e
Sulfur <rc7604@g.rit.edu>
2018-03-05 19:29:07 -0500

    Base
    Made the base parent class for powerups. Also made the base classes and
    methods for the mainmenu. Will fill them in.

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour {
+
+	//gameobject for the main menu items
+	public GameObject mainmenu;
+	//object for credits
+	public GameObject credits;
+	//gameiobject for the instructions
+	public GameObject instructions;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void GoToCredits(){
+
+
+	}
+	public void GoToMainMenu(){
+
+
+	}
+	public void GoToInstructions(){
+
+	}
+	public void Play(){
+
+
+	}
+
+
+}
---
+++ Assets/Scripts/PowerUpParent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class PowerUpParent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//do the power up action and take reference to whatever player has been picked to have 
+	//the actiion preformed upon.
+	public abstract void UsePowerUp (int playerNum);
+}
